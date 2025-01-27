294fc09f675bf69f8defd507ca51f12521420113
Sriyan-Nakka <sriyannakka@gmail.com>
2025-01-27 18:17:37 +0530

    version 0.271

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let turnNumber;
+
 document.querySelector("#playGameButton").onclick = function () {
-  document.querySelector("#playGameSpan").style.display = "none";
+  document.querySelector("#playGameButton").style.display = "none";
   document.querySelector("#gameContainer").style.display = "block";
 };
 
 document.querySelector("#restartGameButton").onclick = function () {
   document.querySelector("#gameContainer").style.display = "none";
-  document.querySelector("#playGameSpan").style.display = "block";
+  document.querySelector("#playGameButton").style.display = "block";
 };
