9476f6dba6e88ec76f5486f8473a9b78344f7921
Sriyan-Nakka <sriyannakka@gmail.com>
2025-04-18 18:42:03 +0530

    0.272

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ document.querySelector("#restartGameButton").onclick = function () {
   document.querySelector("#gameContainer").style.display = "none";
   document.querySelector("#playGameButton").style.display = "block";
 };
+
+document.querySelector("#rock").onclick = function () {
+  console.log("rock clicked");
+};
+document.querySelector("#paper").onclick = function () {
+  console.log("paper clicked");
+};
+document.querySelector("#scissors").onclick = function () {
+  console.log("scissors clicked");
+};
+document.querySelector("#knife").onclick = function () {
+  console.log("knife clicked");
+};
